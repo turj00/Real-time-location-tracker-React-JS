15428282a0d7efc4ac6940784c490390997bd739
turj00 <turjo1331@gmail.com>
2020-09-16 20:30:56 +0600

    Updates

---
+++ precache-manifest.a224b008b843e8ef5f608120d5177904.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a640073d22a77b643edb3ee99c8d981b",
+    "url": "/Real-time-location-tracker-React-JS/index.html"
+  },
+  {
+    "revision": "3f19b882f04961f7279b",
+    "url": "/Real-time-location-tracker-React-JS/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "372202373c5600b0758f",
+    "url": "/Real-time-location-tracker-React-JS/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3f19b882f04961f7279b",
+    "url": "/Real-time-location-tracker-React-JS/static/js/2.20a48a51.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Real-time-location-tracker-React-JS/static/js/2.20a48a51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "372202373c5600b0758f",
+    "url": "/Real-time-location-tracker-React-JS/static/js/main.473cddcd.chunk.js"
+  },
+  {
+    "revision": "cf2134c089cabe4c7c82",
+    "url": "/Real-time-location-tracker-React-JS/static/js/runtime-main.7f97df19.js"
+  }
+]);
